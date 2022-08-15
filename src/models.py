c65cbba371605b5095ab382b1a1de8f6ceb0b813
Mario <waro.brambilla@gmail.com>
2022-08-15 17:03:29 +0000

    relation

--- src/models.py
+++ src/models.py
@@ -8,25 +8,69 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class user(Base):
+    __tablename__ = 'user'
+   
     id = Column(Integer, primary_key=True)
+    email =  Column(String(250), nullable=False)
+    password =  Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class post(Base):
+    __tablename__ = 'post'
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    text = Column(String(250), nullable=True)
 
-    def to_dict(self):
+
+class media(Base):
+    __tablename__ = 'media'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+class like(Base):
+    __tablename__ = 'like'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+class comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+class ads(Base):
+    __tablename__ = 'ads'
+
+    id = Column(Integer, primary_key=True)
+    target_user_id = Column(Integer, ForeignKey('user.id'))
+    author_id = Column(String(250), nullable=False)
+    brand = Column(String(250), nullable=False)
+
+
+class following(Base):
+    __tablename__ = 'following'
+
+    from_id = Column(Integer, ForeignKey('user.id'))
+    to_id = Column(Integer, ForeignKey('user.id'))
+
+
+
+
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
